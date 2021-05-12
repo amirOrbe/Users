6a883e9db1c4cb87172aeaadec99dc9a38aec419
Amir Orbe <orbebrian@gmail.com>
2021-05-12 17:34:25 -0500

    se agrego un case para guardar un usuario

--- apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
+++ apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
@@ -3,11 +3,20 @@ defmodule  BunsanUsersWeb.UserController do
     Controller for Users
     """
     use BunsanUsersWeb, :controller
-
-    def create(conn, _params) do
+    alias BunsanUsers.User
+    def create(conn, %{"user" => user_params}) do
+        chg = User.signup_changeset(%User{}, user_params)
+        case chg.valid? do
+            true ->
+               {:ok, user} = User.save(chg)
         conn
         |> put_status(201)
-        |> json(%{user: "user"})
+        |> json(%{user: %{name: user.name, age: user.age, email: user.email}})
+        false -> 
+            conn 
+            |> put_status(400)
+            |> json(%{error: "Unable to create user"})
+        end
     end
 
     def index(conn, _params) do
