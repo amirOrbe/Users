edc68a8bf9cd3d114b80404840fa1c638844e11f
Amir Orbe <orbebrian@gmail.com>
2021-05-12 17:20:12 -0500

    se agregaron pruebas

--- apps/bunsan_users/test/user_test.exs
+++ apps/bunsan_users/test/user_test.exs
@@ -1,8 +1,33 @@
 defmodule BunsanUsers.UserTest do
     use BunsanUsers.DataCase
+
+    setup do
+        user = %BunsanUsers.User{}
+        user_chg = BunsanUsers.User.signup_changeset(user, %{"name" => "Brian", "age" => 23, "email" => "brian@gmail.com"})
+        stored_user = BunsanUsers.Repo.insert!(user_chg)
+        {:ok, user: stored_user}
+    end
+
+
     test "create user succesfully" do
         user = %BunsanUsers.User{}
-        changeset = BunsanUsers.User.signup_changeset(user, %{name: "Amir", age: 22, email: "orbebrian@gmail.com"})
-        assert 
+        changeset = BunsanUsers.User.signup_changeset(user, %{"name" => "Amir", "age" => 22, "email" => "orbebrian@gmail.com"})
+        assert changeset.valid? == true
+    end
+
+    test "create user unsuccesfully" do
+        user = %BunsanUsers.User{}
+        changeset = BunsanUsers.User.signup_changeset(user, %{"name" => "Amir", "age" => "A", "email" => "orbebrian@gmail.com"})
+        assert changeset.valid? == false
+    end
+
+    test "search user succesfully", %{user: user} do
+        user = BunsanUsers.User.search(user.id)
+        assert user.name == "Brian"
+    end
+
+    test "search all users" do
+       user = BunsanUsers.User.search_all
+       assert users!= []
     end
 end
