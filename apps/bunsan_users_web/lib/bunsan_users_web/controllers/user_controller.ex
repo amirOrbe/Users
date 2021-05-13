9f222f014f5a42622f570038e2d167a581cfc18b
Amir Orbe <orbebrian@gmail.com>
2021-05-13 16:09:06 -0500

    se corrigieron errores

--- apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
+++ apps/bunsan_users_web/lib/bunsan_users_web/controllers/user_controller.ex
@@ -11,23 +11,27 @@ defmodule  BunsanUsersWeb.UserController do
                {:ok, user} = User.save(chg)
         conn
         |> put_status(201)
-        |> json(%{user: %{name: user.name, age: user.age, email: user.email}})
+        |> render("user.json", %{user: user})
         false -> 
-            conn 
-            |> put_status(400)
-            |> json(%{error: "Unable to create user"})
+            {:error, chg}
         end
     end
 
     def index(conn, _params) do
+        users = User.search_all
         conn 
         |> put_status(200)
-        |> json(%{users: []})
+        |> render("users.json", %{users: users})
     end
 
-    def show(conn, _params) do
-        conn
-        |>put_status(200)
-        |> json(%{user: "user"})
+    def show(conn, %{"id" => user_id}) do
+        case User.search(user_id) do
+            nil -> 
+                {:error, "user_not_found"}
+            user ->
+                conn
+                |> put_status(200)
+                |> render("user.json", %{user: user})
+        end
     end
 end
